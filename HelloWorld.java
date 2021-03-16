d26d17d8115df22759032c68ca7ed81d82c69845
GVANT$A <gvantsa.beridze.2@btu.edu.ge>
2021-03-17 00:49:50 +0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+package com.example.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        String [] familyMembers = {"Gvantsa", "Givi", "laptops"};
+
+        for (String name : familyMembers) {
+            System.out.println(name);
+        }
+
+
+    }
+
+}
